9ebcc1ac1b7c89d1bc8e9570ca7163ede2f2f59c
Jonathan Knapp <jonathan@purse.io>
2017-07-13 21:13:39 -0700

    Added ContextMenu to extension

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -7,12 +7,30 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 // Send current tabs url to MPV server
 chrome.pageAction.onClicked.addListener(function(tab){
     // console.info("Clicked!", tab.url)
+    playUrl(tab.url);
+});
+
+function playUrl(url) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = handleXHR;
-    xhr.open("GET", "http://localhost:7531/?play_url="+tab.url, true);
+    xhr.open("GET", "http://localhost:7531/?play_url="+url, true);
     xhr.send();
-});
+}
 
 function handleXHR() {
   // console.log("XHR", arguments)
 }
+
+var parent = chrome.contextMenus.create({
+	"id": "1",
+	"title": "Play with MPV",
+	"contexts": ["page", "link", "video", "audio"]
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+	// console.log("item " + info.menuItemId + " was clicked");
+	// console.log("info: " + JSON.stringify(info));
+	// console.log("tab: " + JSON.stringify(tab));
+
+	playUrl(info["linkUrl"] || info["srcUrl"]|| info["pageUrl"]);
+});
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 import sys
 from subprocess import Popen
 
