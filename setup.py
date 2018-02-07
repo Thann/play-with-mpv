a8b5f7598480d988998c048eb62379c6fb855ed0
Jonathan Knapp <jonathan@purse.io>
2018-02-07 14:01:30 -0800

    Added description to free_desktop file

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import setup, find_packages
 
+description = "Chrome extension and python server that allows you to play videos in webpages with MPV instead."
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -9,7 +11,7 @@ setup(
     version = "0.0.6",
     author = "Jonathan Knapp",
     author_email = "jaknapp8@gmail.com",
-    description = "Chrome extension and python server that allows you to play videos in webpages with MPV instead.",
+    description = description,
     license = "MIT",
     keywords = "mpv video play chrome extension",
     url = "http://github.com/thann/play-with-mpv",
@@ -36,6 +38,7 @@ setup(
             'filename': 'thann.play-with-mpv',
             'Name': 'Play With MPV (server)',
             'Categories': 'AudioVideo;Audio;Video;Player;TV',
+            'Comment': description,
             'Icon': 'mpv',
         },
     },
